81d9897be770d6bbd63f977f3b06e8065762e802
Gabriel Guilherme <gabriel.guilherme.131@ufrn.edu.br>
2025-06-14 19:19:46 -0300

    broker refactor

--- src/main/java/application/MainApplication.java
+++ src/main/java/application/MainApplication.java
@@ -1,21 +1,16 @@
 package application;
 
 import middleware.broker.Broker;
-import middleware.server.ServerRequestHandler;
-import middleware.invoker.Invoker;
-import middleware.lookup.LookupService;
-import middleware.marshaller.JsonMarshaller;
+
 
 public class MainApplication {
     public static void main(String[] args) {
-        LookupService lookup = new LookupService();
-        JsonMarshaller marshaller = new JsonMarshaller();
-        Invoker invoker = new Invoker(lookup, marshaller);
-        ServerRequestHandler server = new ServerRequestHandler(invoker, marshaller);
-        Broker broker = new Broker(lookup, server);
+
+        Broker broker = new Broker();
 
         Calculator calc = new Calculator();
-        lookup.register("Calculator", calc);
+        
+        broker.register("calculator", calc);
 
         broker.start();
     }
--- src/main/java/middleware/broker/Broker.java
+++ src/main/java/middleware/broker/Broker.java
@@ -3,18 +3,29 @@ package middleware.broker;
 import middleware.lookup.LookupService;
 import middleware.server.ServerRequestHandler;
 
+
+
+import middleware.invoker.Invoker;
+
+import middleware.marshaller.JsonMarshaller;
+
 public class Broker {
 
-    private final LookupService lookup;
-    private final ServerRequestHandler server;
+    public final LookupService lookup = new LookupService();
 
-    public Broker(LookupService lookup, ServerRequestHandler server) {
-        this.lookup = lookup;
-        this.server = server;
-    }
+    private final JsonMarshaller marshaller = new JsonMarshaller();
+    private final Invoker invoker = new Invoker(lookup, marshaller);
+    private final ServerRequestHandler server = new ServerRequestHandler(invoker, marshaller);
+
+
+    public Broker() {}
 
     public void start() {
         server.start();
+    }
+
+    public void register(String serviceName, Object service) {
+        lookup.register(serviceName, service);
     }
 
     public LookupService getLookup() {
